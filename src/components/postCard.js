d960cf0f37cc17654ea421e9541a664f82388fd2
Tim Zeng <zengtiantian1122@gmail.com>
2017-11-14 22:55:03 -0800

    create PostCard and Loading components

---
+++ src/components/postCard.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const PostCard = ({ post }) => (
+  <div className='post-card'>
+    <div className='title'>
+      <a href={post.data.url} target='__blank'>{post.data.title}</a>
+    </div>
+
+    <p className='info light-text'>
+      <span>by&nbsp;
+        <a
+          href={`https://www.reddit.com/user/${post.data.author}`}
+          target='__blank'
+        >
+          {post.data.author}
+        </a>
+      </span>
+      &nbsp;&middot;&nbsp;
+      <span>{post.data.num_comments} comments</span>
+      &nbsp;&middot;&nbsp;
+      <span>{post.data.score} votes</span>
+      &nbsp;&middot;&nbsp;
+      <span>{post.data.subreddit}</span>
+    </p>
+
+  </div>
+);
+
+export default PostCard;
--- src/containers/app.js
+++ src/containers/app.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
+import PostCard from '../components/postCard';
+import Loading from '../components/loading';
 import { fetchPosts } from '../actions';
 
 class App extends Component {
@@ -10,11 +12,27 @@ class App extends Component {
   }
 
   render() {
+    const { posts } = this.props;
+
     return (
-      <div className='container'>
-        {
-          this.props.posts.map(post => <p key={post.data.id}>{post.data.title}</p>)
-        }
+      <div>
+        <header>
+          <img src='https://seeklogo.com/images/R/reddit-logo-8ABF8F5F2B-seeklogo.com.png'/>
+          <span>Reddit Reader</span>
+        </header>
+
+        <div className='container'>
+          {
+            posts.length === 0
+            ? <Loading />
+            : null
+          }
+          {
+            posts.map(post => (
+              <PostCard key={post.data.id} post={post} />
+            ))
+          }
+        </div>
       </div>
     )
   }
